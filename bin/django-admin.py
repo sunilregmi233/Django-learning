7e7d527ef802cc82ca8cc50fa1bccb94d4fac2a3
sunilregmi233 <sunilregmi223@gmail.com>
2019-04-09 20:13:29 +0545

    Hello world

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/sunil/Entertaintment/work/jpaitai/django app/Django-learning/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
